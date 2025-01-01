6497db4e57a5b1eba22acc48f9e2d39b807238ab
Omar Salem <UGS.140670@ci.suez.edu.eg>
2025-01-01 10:35:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
