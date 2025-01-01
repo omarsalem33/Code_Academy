0848199dee3ad1a65e68bcc14a2f48ba6a057c8d
Omar Salem <UGS.140670@ci.suez.edu.eg>
2025-01-01 13:00:52 +0200

    Add last Migration

---
+++ Migrations/20250101105930_Add Relation betwwen Schdules and Sections(N-M).cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeAcademy.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelationbetwwenSchdulesandSectionsNM : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScheduleSection",
+                columns: table => new
+                {
+                    SchedulesId = table.Column<int>(type: "int", nullable: false),
+                    SectionsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleSection", x => new { x.SchedulesId, x.SectionsId });
+                    table.ForeignKey(
+                        name: "FK_ScheduleSection_Schedules_SchedulesId",
+                        column: x => x.SchedulesId,
+                        principalTable: "Schedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ScheduleSection_Sections_SectionsId",
+                        column: x => x.SectionsId,
+                        principalTable: "Sections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SectionSchedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    EndTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    ScheduleId = table.Column<int>(type: "int", nullable: false),
+                    SectionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SectionSchedules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SectionSchedules_Schedules_ScheduleId",
+                        column: x => x.ScheduleId,
+                        principalTable: "Schedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SectionSchedules_Sections_SectionId",
+                        column: x => x.SectionId,
+                        principalTable: "Sections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleSection_SectionsId",
+                table: "ScheduleSection",
+                column: "SectionsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectionSchedules_ScheduleId",
+                table: "SectionSchedules",
+                column: "ScheduleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectionSchedules_SectionId",
+                table: "SectionSchedules",
+                column: "SectionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleSection");
+
+            migrationBuilder.DropTable(
+                name: "SectionSchedules");
+        }
+    }
+}
