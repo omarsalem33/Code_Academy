41d5390cb023c42fc6386b04c8a8b7e6281625de
Omar Salem <UGS.140670@ci.suez.edu.eg>
2025-01-01 11:46:32 +0200

    Edit data for Instructors

--- Data/Config/InstructorConfiguration .cs
+++ Data/Config/InstructorConfiguration .cs
@@ -38,11 +38,11 @@ namespace CodeAcademy.Data.Config
         {
             return new List<Instructor>
             {
-                new Instructor { Id = 1, FName = "Ahmed", LName = "Abdullah"},
-                new Instructor { Id = 2, FName = "Yasmeen", LName = "Yasmeen"},
-                new Instructor { Id = 3, FName = "Khalid", LName = "Hassan"},
-                new Instructor { Id = 4, FName = "Nadia", LName = "Ali"},
-                new Instructor { Id = 5, FName = "Ahmed", LName = "Abdullah"},
+                new Instructor { Id = 1, FName = "Ahmed", LName = "Abdullah", OfficeId = 1},
+                new Instructor { Id = 2, FName = "Yasmeen", LName = "Yasmeen", OfficeId = 2},
+                new Instructor { Id = 3, FName = "Khalid", LName = "Hassan", OfficeId = 3},
+                new Instructor { Id = 4, FName = "Nadia", LName = "Ali", OfficeId = 4},
+                new Instructor { Id = 5, FName = "Ahmed", LName = "Abdullah", OfficeId = 5},
             };
         }
     }
