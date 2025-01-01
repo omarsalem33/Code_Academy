6ec0cd258dc26cb449b5366f687c3d7d6e0c5946
Omar Salem <UGS.140670@ci.suez.edu.eg>
2025-01-01 11:02:02 +0200

    Add Course Configuration

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CodeAcademy.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace CodeAcademy.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Course> Courses { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
@@ -14,6 +17,13 @@ namespace CodeAcademy.Data
             var connectionString = config.GetSection("ConnectionStrings:DefaultConnection").Value;
 
             optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         }
     }
 }
---
+++ Data/Config/CourseConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using CodeAcademy.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAcademy.Data.Config
+{
+    public class CourseConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedNever();
+
+            builder.Property(x => x.CourseName)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(50).IsRequired();
+
+            builder.HasData(LoadCourses());
+
+            builder.ToTable("Courses");
+
+        }
+
+        private static List<Course> LoadCourses()
+        {
+            return new List<Course>
+            {
+                new Course { Id = 1, CourseName = "Mathmatics", Price = 1000m},
+                new Course { Id = 2, CourseName = "Physics", Price = 2000m},
+                new Course { Id = 3, CourseName = "Chemistry", Price = 1500m},
+                new Course { Id = 4, CourseName = "Biology", Price = 1200m},
+                new Course { Id = 5, CourseName = "CS-50", Price = 3000m},
+            };
+        }
+    }
+}
