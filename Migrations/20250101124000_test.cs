1b45219aa6f8a2c7d3cb8f5628d6787049072648
Omar Salem <UGS.140670@ci.suez.edu.eg>
2025-01-01 17:54:58 +0200

    add all migration

---
+++ Migrations/20250101124000_test.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeAcademy.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScheduleSection");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ScheduleSection",
+                columns: table => new
+                {
+                    SchedulesId = table.Column<int>(type: "int", nullable: false),
+                    SectionsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScheduleSection", x => new { x.SchedulesId, x.SectionsId });
+                    table.ForeignKey(
+                        name: "FK_ScheduleSection_Schedules_SchedulesId",
+                        column: x => x.SchedulesId,
+                        principalTable: "Schedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ScheduleSection_Sections_SectionsId",
+                        column: x => x.SectionsId,
+                        principalTable: "Sections",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScheduleSection_SectionsId",
+                table: "ScheduleSection",
+                column: "SectionsId");
+        }
+    }
+}
