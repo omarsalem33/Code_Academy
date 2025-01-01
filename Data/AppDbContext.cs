8123ad76e70c86789c9bdaa407b15e73f42fba78
Omar Salem <UGS.140670@ci.suez.edu.eg>
2025-01-01 10:46:11 +0200

    add configuration of connection string

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CodeAcademy.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = config.GetSection("ConnectionStrings:DefaultConnection").Value;
+        }
+    }
+}
